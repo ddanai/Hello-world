be8043378360b760615acfb39d2bf3ce3235bffe
Danai <ddanai@friendsbalt.org>
2023-01-27 15:33:17 -0500

    initial commit

---
+++ Documents/ACSI/bubbleMain.java
@@ -0,0 +1,7 @@
+public class bubbleMain{
+    int[]arr = {-17,-3,0,-4,2,-6,-19,8,18,18};
+    int[]sorted = sort(int[]arr);
+    for int i; i<sorted.length; i++;{
+        System.out.println(sorted[i]);
+    }
+}
